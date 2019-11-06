5157367d0fa8e73ee5170b6438d585452bfd4170
prateek-pk27 <30526910+prateek-pk27@users.noreply.github.com>
2019-11-07 00:09:14 +0530

    Update toh.cpp

--- cpp/toh.cpp
+++ cpp/toh.cpp
@@ -1,21 +1,19 @@
 #include<iostream>
 using namespace std;
-int toh(int n,int l,int m,int r)
-{	
-	if(n==1)
-	{cout<<"("<<l<<","<<r<<")\n";
-	l=r;}
-	else
-	{
-	toh((n-1),l,r,m);
-	cout<<"("<<l<<","<<r<<")\n";
-	toh((n-1),m,l,r);
+int toh(int n,int l,int m,int r){	
+	if(n==1){
+		cout<<"("<<l<<","<<r<<")\n";
+		l=r;
+	}
+	else{
+		toh((n-1),l,r,m);
+		cout<<"("<<l<<","<<r<<")\n";
+		toh((n-1),m,l,r);
 	}
 	
 }
 
-int main()
-{
+int main(){
 	int N,L=1,M=2,R=3;
 	cout<<"enter no. of disks in tower of hanoi ";
 	cin>>N;
