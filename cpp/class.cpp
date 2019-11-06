d15127074b280a4f8cee8a6f8d1fa58df9e20397
prateek-pk27 <30526910+prateek-pk27@users.noreply.github.com>
2019-11-07 00:21:57 +0530

    Update class.cpp

--- cpp/class.cpp
+++ cpp/class.cpp
@@ -1,28 +1,25 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class cse
-{
-int rollno;
-std::name;
-public:
-	void getdata()
-	{
-	cout<<"Enter Roll No. ";
-	cin>>rollno;
-	cout<<"Enter Name ";
-	cin>>name;
-	}
-	void showdata()
-	{
-	cout<<"Roll No.:"<<rollno<<"\n";
-	cout<<"Name:"<<name<<"\n";
-	}
-	};
-int main()
-{
-cse s;
-s.getdata();
-s.showdata();
+class cse {
+	int rollno;
+	std::name;
+	public:
+		void getdata(){
+			cout<<"Enter Roll No. ";
+			cin>>rollno;
+			cout<<"Enter Name ";
+			cin>>name;
+		}
+		void showdata(){
+			cout<<"Roll No.:"<<rollno<<"\n";
+			cout<<"Name:"<<name<<"\n";
+		}
+};
+
+int main(){
+	cse s;
+	s.getdata();
+	s.showdata();
 }
 
