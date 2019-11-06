17df1cf9b42757b0d59e4566aaef607d5d2d1085
prateek-pk27 <30526910+prateek-pk27@users.noreply.github.com>
2019-11-07 00:10:27 +0530

    Update squares.cpp

--- cpp/squares.cpp
+++ cpp/squares.cpp
@@ -1,19 +1,17 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-int a,b;
-int sq=0;
-cout<<"enter a range of no. to find no. of perfect squares between them \n";
-cin>>a>>b;
-for(int i=a;i<=b;i++)
-	{
-	for(int x=1;x<=i/2;x++)
-		{if(i==x*x)
-			{cout<<i<<" is a perfect square \n";
-			sq++;
+int main(){
+	int a,b;
+	int sq=0;
+	cout<<"enter a range of no. to find no. of perfect squares between them \n";
+	cin>>a>>b;
+	for(int i=a;i<=b;i++){
+		for(int x=1;x<=i/2;x++){
+			if(i==x*x){
+				cout<<i<<" is a perfect square \n";
+				sq++;
 			}
 		}
 	}
-cout<<"total no. of perfect squares between "<<a<<" and "<<b<<" are "<<sq<<"\n";
+	cout<<"total no. of perfect squares between "<<a<<" and "<<b<<" are "<<sq<<"\n";
 }
